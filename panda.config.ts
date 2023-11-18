5c91c8618db38fb53d58111fd6e0fb909daaac65
motoki <motoki610972@icloud.com>
2023-11-18 22:23:12 +0900

    feat: レスポンシブ設定

--- panda.config.ts
+++ panda.config.ts
@@ -1,23 +1,25 @@
 import { defineConfig } from '@pandacss/dev';
 
 export default defineConfig({
-  // Whether to use css reset
   preflight: true,
 
-  // Where to look for your css declarations
   include: [
     './src/components/**/*.{ts,tsx,js,jsx}',
     './src/app/**/*.{ts,tsx,js,jsx}',
   ],
 
-  // Files to exclude
   exclude: [],
 
-  // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      breakpoints: {
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1110px',
+      },
+    },
   },
 
-  // The output directory for your css system
   outdir: 'styled-system',
 });
