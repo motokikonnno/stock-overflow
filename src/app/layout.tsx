72ad8193270e77fa4708a025d331a66ca3e05577
motoki <motoki610972@icloud.com>
2023-12-03 23:07:27 +0900

    feat: mantine導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import 'src/app/globals.css';
 
+import { ColorSchemeScript } from '@mantine/core';
+
 import { ClientProviders } from './ClientProviders';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html>
+      <head>
+        <ColorSchemeScript />
+      </head>
       <body>
         <ClientProviders>{children}</ClientProviders>
       </body>
